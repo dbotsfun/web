3e3084ba03fd3bc71fe2b3b04d8880be97a49794
simxnet <simxnet@envs.net>
2024-02-24 01:33:56 +0100

    fix: i said dynamic url

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,4 @@
-const LOGIN_URL = "https://discord.com/oauth2/authorize?client_id=1197575977333702676&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback&scope=identify"
+const LOGIN_URL = `https://discord.com/oauth2/authorize?client_id=1197575977333702676&response_type=code&redirect_uri=${encodeURIComponent(`${process.env.NEXT_PUBLIC_URL}/api/auth/callback`)}&scope=identify`
 
 export async function GET() {
     return Response.redirect(LOGIN_URL)
