0e542bb87b6c18e4160caac51526ba597150b9c2
simxnet <simxnet@envs.net>
2024-03-02 13:13:07 +0100

    feat(seo): update color theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,7 @@ const font = FontSans({
 })
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
+  themeColor: "#6271fe",
 }
 
 export default function RootLayout({
